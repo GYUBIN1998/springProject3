0b13015476e9f028842daafe484115f5f9d80a79
SOL AN <desorrh@gmail.com>
2022-08-17 13:59:43 +0900

    DTO(user, category, notebook, shipping)생성

---
+++ springProject/src/main/java/com/group3/springProject/dto/User.java
@@ -0,0 +1,23 @@
+package com.group3.springProject.dto;
+
+import lombok.Data;
+
+/*
++-------------+--------------+------+-----+---------+-------+
+| Field       | Type         | Null | Key | Default | Extra |
++-------------+--------------+------+-----+---------+-------+
+| user_id     | varchar(255) | NO   | PRI | NULL    |       |
+| user_pw     | varchar(255) | NO   |     | NULL    |       |
+| user_phone  | varchar(255) | NO   | UNI | NULL    |       |
+| user_email  | varchar(255) | NO   | UNI | NULL    |       |
+| user_adress | varchar(255) | NO   |     | NULL    |       |
++-------------+--------------+------+-----+---------+-------+
+*/
+@Data
+public class User {
+	private String user_id;
+	private String user_pw;
+	private String user_phone;
+	private String user_email;
+	private String user_adress; 
+}
