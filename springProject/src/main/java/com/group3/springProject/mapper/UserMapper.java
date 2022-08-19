a4ab6ec6960f419172b90b96f0244d334c583df8
Ran <pwr131008@gmail.com>
2022-08-19 11:26:02 +0900

    main에 R 작업 내역 추가

---
+++ springProject/src/main/java/com/group3/springProject/mapper/UserMapper.java
@@ -0,0 +1,12 @@
+package com.group3.springProject.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+
+//com.group3.springProject.mapper.UserMapper
+
+@Mapper
+public interface UserMapper {
+	
+}
